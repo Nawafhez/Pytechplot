ac04ac354447a9c2474cb2457763310a4541cb19
Nawafhez <121361744+Nawafhez@users.noreply.github.com>
2023-11-13 22:37:42 -0500

    2

--- tecplot_test.py
+++ tecplot_test.py
@@ -1,5 +1,11 @@
 import tecplot as tp
 from tecplot.constant import SliceSource, SliceSurface, PlotType, AxisMode, TransientOperationMode
+import pandas as pd
+
+#Argparse
+
+# Connect to a running instance of Tecplot 360 if needed
+tp.session.connect()
 
 tp.macro.execute_command("""$!ReadDataSet  '\"/storage/home/nka5267/work/OneraM6_SU2_RANS.plt\" '
   ReadDataOption = New
@@ -8,34 +14,37 @@ tp.macro.execute_command("""$!ReadDataSet  '\"/storage/home/nka5267/work/OneraM6
   AssignStrandIDs = Yes
   VarNameList = '\"x\" \"y\" \"z\" \"Density\" \"Momentum U (Density*U)\" \"Momentum V (Density*V)\" \"Momentum W (Density*W)\" \"Energy (Density*E)\" \"SA Turbulent Eddy Viscosity\" \"Pressure\" \"Temperature\" \"Pressure_Coefficient\" \"Mach\" \"Laminar_Viscosity\" \"Skin_Friction_Coefficient\" \"Heat_Flux\" \"Y_Plus\" \"Eddy_Viscosity\"'""")
 
-
-# Connect to a running instance of Tecplot 360 if needed
-# tp.session.connect()
-
 # Get the active frame and its plot
 frame = tp.active_frame()
+frame.plot_type = PlotType.Cartesian3D
 plot = frame.plot()
+
 
 # Set contour variables & colormap
 plot.contour(0).variable_index = 1
 plot.contour(0).colormap_name = 'Modern'
 
 # Show contour and slices
-plot.show_contour = True
+plot.show_contour = False
 plot.show_slices = True
 
+# Get the dataset # Pandas
+dataset = tp.active_frame().dataset
+y= dataset.variable(1)
+
 # Set slices properties
-y_positions = [0.0425727, 0.316254, 0.604532, 0.935383, 1.17379]
+y_positions = [0, y/4, y/2 , 3*y/4, y]
+
 for i, y in enumerate(y_positions):
     slice_ = plot.slice(i)
+    slice_.show = True
     slice_.slice_source = SliceSource.SurfaceZones
     slice_.orientation = SliceSurface.YPlanes
     slice_.origin.y = y
-    slice_.edge.show = True
+    slice_.edge.show = False
     slice_.mesh.show = True
     slice_.mesh.color = plot.contour(0)
     slice_.mesh.line_thickness = 0.8
-    slice_.edge.show = False
 
 
 # Extract slices
@@ -44,11 +53,6 @@ plot.slices(0,1,2,3,4).extract(transient_mode=TransientOperationMode.AllSolution
 # Update the view of the plot
 plot.view.position = (6.96919, plot.view.position[1], plot.view.position[2])
 plot.view.width = 1.62394
-
-# Update axes scales
-x_axis_scale_factors = [1.1, 1.21, 1.331, 1.4641, 1.61051, 1.77156, 1.94872, 2.14359, 2.35795, 2.59374]
-for scale in x_axis_scale_factors:
-    plot.axes.x_axis.scale_factor = scale
 
 # Redraw the plot
 tp.macro.execute_command('$!RedrawAll')
@@ -59,16 +63,16 @@ plot.axes.z_axis.show = True
 plot.axes.axis_mode = AxisMode.Independent
 
 # Define the variable indices for Cp and x
-cp_var_index = 11  # replace with the actual index for Cp
-x_var_index = 0   # replace with the actual index for x
+cp_var_index = 11  
+x_var_index = 0   
 
-# Get the dataset
-dataset = tp.active_frame().dataset
+
 
 # Loop to create a new frame and plot for each Cp graph
-for i in range(number_of_graphs):  # Replace with the actual number of Cp graphs
+for i in range(5):
+
     # Create a new frame
-    frame = tp.active_frame().add_frame()
+    frame = tp.active_page().add_frame()
     
     # Set the frame mode to 2D
     frame.plot_type = PlotType.XYLine
@@ -93,16 +97,15 @@ for i in range(number_of_graphs):  # Replace with the actual number of Cp graphs
     line.line.line_thickness = 0.8
     
     # Optionally, set the title of the graph to something descriptive
-    plot.title = f"Cp vs. x Graph {i+1}"
+    plot.title = f" y{i}"
     
     # Redraw the frame
     frame.draw()
 
-# Activate the first frame at the end
 tp.active_page().frames[0].activate()
 
-
 # Final redraw of the plot
+plot.view.fit()
 tp.macro.execute_command('$!RedrawAll')
 
 # End of script
